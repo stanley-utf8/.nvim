6e20d74e3e048cb4287a31ce808271303ea9d17d
twinfantasyfan <wangstanley910@gmail.com>
2025-01-07 22:01:52 -0500

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,24 @@
 return {
-  -- First item
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = false,
-  --   name = "catppuccin",
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       flavour = "mocha", -- latte, frappe, macchiato, mocha
-  --     })
-  --   end,
-  -- },
-  -- Second item
   {
-    "gambhirsharma/vesper.nvim",
+    "catppuccin/nvim",
     lazy = false,
-    priority = 1000,
-    name = "vesper",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+      })
+    end,
   },
-  -- Third item
+  -- {
+  --   "gambhirsharma/vesper.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   name = "vesper",
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vesper",
+      colorscheme = "catppuccin",
     },
   },
 }
